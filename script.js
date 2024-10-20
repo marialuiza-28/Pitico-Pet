0e7606165d9300d37b3b4070455dfb236dc715b2
marialuiza-28 <marialuizaalexandre289@gmail.com>
2024-10-20 15:42:53 -0300

    Pitico Pet

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
 
-console.log('Página de cadastro')
-
-function validarFormulario() {
-    const nome = document.getElementById("nome").value;
-    if (nome === "") {
-        alert("Por favor, preencha o nome.");
-        return false;
-    }
-    return true;
-}
 
